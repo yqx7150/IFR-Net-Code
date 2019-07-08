63dc9344161f72bfb0ab65f29af3d07dd042451d
Qingxin Yang <yqx7150@163.com>
2019-07-08 14:02:48 +0800

    Add files via upload

---
+++ matconvnet/setup.m
@@ -0,0 +1,4 @@
+% This setup is tested under Ubuntu 14.04. CUDNN needs to be copied into local directory
+addpath matlab
+%vl_compilenn('enableGPU', 1, 'cudaRoot', '/usr/local/cuda-7.0', 'cudaMethod', 'nvcc', 'enableCudnn', 1, 'cudnnRoot', 'local/');
+vl_compilenn('enableGPU', 1, 'cudaMethod', 'nvcc');
